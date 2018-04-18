eee5056101404d4ea9221c87122bc15db7422663
狂奔的唐玄奘 <1310683823@qq.com>
2018-04-18 11:19:15 +0800

    fix: ( #1891 ) 日志打印样式调整

--- common/src/main/java/com/zhiyicx/common/utils/log/LogUtils.java
+++ common/src/main/java/com/zhiyicx/common/utils/log/LogUtils.java
@@ -1,7 +1,14 @@
 package com.zhiyicx.common.utils.log;
 
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.SystemClock;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
 import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.FormatStrategy;
+import com.orhanobut.logger.LogStrategy;
 import com.orhanobut.logger.Logger;
 import com.orhanobut.logger.PrettyFormatStrategy;
 
@@ -23,7 +30,7 @@ public class LogUtils {
 //                .showThreadInfo(false)  // (Optional) Whether to show thread info or not. Default true
                 .methodCount(LOGGER_METHODCOUNT)         // (Optional) How many method line to show. Default 2
                 .methodOffset(LOGGER_METHODOFFSET)        // (Optional) Hides internal method calls up to offset. Default 5
-//                .logStrategy(customLog) // (Optional) Changes the log strategy to print out. Default LogCat
+                .logStrategy(new LogCatStrategy()) // (Optional) Changes the log strategy to print out. Default LogCat
                 .tag(APPLICATION_TAG)   // (Optional) Global tag for every log. Default PRETTY_LOGGER
                 .build();
 
@@ -99,5 +106,34 @@ public class LogUtils {
 
     public static void xml(String tag, String xml) {
         Logger.t(tag).xml(xml);
+    }
+
+    public static class LogCatStrategy implements LogStrategy {
+
+        private Handler handler;
+        private long lastTime = SystemClock.uptimeMillis();
+        private long offset = 5;
+
+        public LogCatStrategy() {
+            HandlerThread thread = new HandlerThread("thread_print");
+            thread.start();
+            handler = new Handler(thread.getLooper());
+        }
+
+        @Override
+        public void log(final int priority, final String tag, @NonNull final String message) {
+
+            lastTime += offset;
+            if (lastTime < SystemClock.uptimeMillis()) {
+                lastTime = SystemClock.uptimeMillis() + offset;
+            }
+            final long tmp = lastTime;
+            handler.postAtTime(new Runnable() {
+                @Override
+                public void run() {
+                    Log.println(priority, tag, message);
+                }
+            }, tmp);
+        }
     }
 }
