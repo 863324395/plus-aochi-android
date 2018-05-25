a1e391f5445a0c4878678bd061d32786d5fcb5b7
狂奔的唐玄奘 <1310683823@qq.com>
2018-05-25 19:12:02 +0800

    feat: ( #1324 ) 找人搜索页的推荐调整

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/findsomeone/search/name/SearchSomeOneFragment.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/findsomeone/search/name/SearchSomeOneFragment.java
@@ -1,6 +1,9 @@
 package com.zhiyicx.thinksnsplus.modules.findsomeone.search.name;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.View;
@@ -8,22 +11,30 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.jakewharton.rxbinding.view.RxView;
 import com.jakewharton.rxbinding.widget.RxTextView;
 import com.zhiyicx.baseproject.base.TSListFragment;
 import com.zhiyicx.baseproject.widget.edittext.DeleteEditText;
+import com.zhiyicx.common.utils.ConvertUtils;
 import com.zhiyicx.common.utils.DeviceUtils;
 import com.zhiyicx.thinksnsplus.R;
 import com.zhiyicx.thinksnsplus.data.beans.UserInfoBean;
 import com.zhiyicx.thinksnsplus.modules.findsomeone.list.FindSomeOneListAdapter;
+import com.zhiyicx.thinksnsplus.modules.personal_center.PersonalCenterFragment;
+import com.zhiyicx.thinksnsplus.utils.ImageUtils;
 import com.zhy.adapter.recyclerview.CommonAdapter;
 import com.zhy.adapter.recyclerview.MultiItemTypeAdapter;
+import com.zhy.adapter.recyclerview.base.ViewHolder;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.OnClick;
+
+import static com.zhiyicx.common.config.ConstantConfig.JITTER_SPACING_TIME;
 
 /**
  * @author Jungle68
@@ -44,8 +55,15 @@ public class SearchSomeOneFragment extends TSListFragment<SearchSomeOneContract.
     RelativeLayout mFragmentInfoSearchContainer;
     @BindView(R.id.fragment_search_cancle)
     TextView mTvSearchCancel;
+    @BindView(R.id.tv_recomment_user)
+    TextView mRecommentUser;
     @BindView(R.id.ll_container)
     View mLlContainer;
+
+    private GridLayoutManager mGridLayoutManager;
+    private boolean useGridLayoutManager = true;
+    private CommonAdapter<UserInfoBean> mRecommentAdapter;
+    private FindSomeOneListAdapter mFindSomeOneListAdapter;
 
 
     public static SearchSomeOneFragment newInstance(Bundle args) {
@@ -84,12 +102,26 @@ public class SearchSomeOneFragment extends TSListFragment<SearchSomeOneContract.
     protected void initView(View rootView) {
         super.initView(rootView);
         setEmptyViewVisiable(false);
+
+        mFindSomeOneListAdapter = new FindSomeOneListAdapter(getActivity(), R.layout.item_find_some_list, mListDatas, mPresenter);
+        mFindSomeOneListAdapter.setOnItemClickListener(this);
+
+
         RxTextView.editorActionEvents(mFragmentInfoSearchEdittext).subscribe(textViewEditorActionEvent -> {
             if (textViewEditorActionEvent.actionId() == EditorInfo.IME_ACTION_SEARCH) {
-                if (!TextUtils.isEmpty(textViewEditorActionEvent.toString().trim())) {
+                if (!TextUtils.isEmpty(mFragmentInfoSearchEdittext.getText().toString().trim())) {
                     mPresenter.searchUser(mFragmentInfoSearchEdittext.getText().toString().trim());
                     DeviceUtils.hideSoftKeyboard(getContext(), mFragmentInfoSearchEdittext);
+                    useGridLayoutManager = false;
+                    mRvList.setAdapter(mFindSomeOneListAdapter);
+                    mRvList.setLayoutManager(new LinearLayoutManager(mActivity));
+                    mRootView.setBackgroundColor(getColor(R.color.bgColor));
+                    mRecommentUser.setVisibility(View.GONE);
                 } else {
+                    useGridLayoutManager = true;
+                    mRvList.setAdapter(mRecommentAdapter);
+                    mRvList.setLayoutManager(mGridLayoutManager);
+                    mRootView.setBackgroundColor(getColor(R.color.white));
                     mPresenter.getRecommentUser();
                 }
 
@@ -114,6 +146,20 @@ public class SearchSomeOneFragment extends TSListFragment<SearchSomeOneContract.
     }
 
     @Override
+    protected float getItemDecorationSpacing() {
+        return useGridLayoutManager ? 0 : super.getItemDecorationSpacing();
+    }
+
+    @Override
+    public void onNetResponseSuccess(@NotNull List<UserInfoBean> data, boolean isLoadMore) {
+        super.onNetResponseSuccess(data, isLoadMore);
+        mFindSomeOneListAdapter.notifyDataSetChanged();
+        if (useGridLayoutManager) {
+            mRecommentUser.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
     protected boolean showToolbar() {
         return false;
     }
@@ -132,11 +178,30 @@ public class SearchSomeOneFragment extends TSListFragment<SearchSomeOneContract.
     }
 
     @Override
-    protected CommonAdapter getAdapter() {
-        CommonAdapter adapter = new FindSomeOneListAdapter(getActivity(), R.layout.item_find_some_list, mListDatas, mPresenter);
-        adapter.setOnItemClickListener(this);
-        return adapter;
+    protected RecyclerView.LayoutManager getLayoutManager() {
+        mGridLayoutManager = new GridLayoutManager(mActivity, 2);
+        return mGridLayoutManager;
+    }
 
+    @Override
+    protected RecyclerView.Adapter getAdapter() {
+        mRecommentAdapter = new CommonAdapter<UserInfoBean>(mActivity, R.layout.item_find_some_recomment_list, mListDatas) {
+            @Override
+            protected void convert(ViewHolder holder, UserInfoBean userInfoBean, int position) {
+                // 头像加载
+                ImageUtils.loadCircleUserHeadPic(userInfoBean, holder.getView(R.id.iv_headpic));
+                // 添加点击事件
+                RxView.clicks(holder.getConvertView())
+                        .throttleFirst(JITTER_SPACING_TIME, TimeUnit.SECONDS)
+                        .subscribe(aVoid -> toUserCenter(getContext(), userInfoBean));
+
+                // 设置用户名，用户简介
+                holder.setText(R.id.tv_name, userInfoBean.getName());
+                String followingCount = String.valueOf(userInfoBean.getExtra().getFollowings_count());
+                holder.setText(R.id.tv_user_signature, getString(R.string.fans_count, ConvertUtils.numberConvert(Integer.parseInt(followingCount))));
+            }
+        };
+        return mRecommentAdapter;
     }
 
     @Override
@@ -169,5 +234,12 @@ public class SearchSomeOneFragment extends TSListFragment<SearchSomeOneContract.
     @Override
     protected Long getMaxId(@NotNull List<UserInfoBean> data) {
         return (long) mListDatas.size();
+    }
+
+    private void toUserCenter(Context context, UserInfoBean userInfoBean) {
+        if (mPresenter.handleTouristControl()) {
+            return;
+        }
+        PersonalCenterFragment.startToPersonalCenter(context, userInfoBean);
     }
 }
