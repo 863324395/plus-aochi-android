8691a1c24d64375ab057cd536437aff7429f62ac
狂奔的唐玄奘 <1310683823@qq.com>
2018-05-30 17:17:59 +0800

    feat: ( #1324 ) 黑名单文字 format 修改

--- app/src/main/java/com/zhiyicx/thinksnsplus/modules/circle/manager/members/MemberListFragment.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/circle/manager/members/MemberListFragment.java
@@ -291,7 +291,7 @@ public class MemberListFragment extends TSListFragment<MembersContract.Presenter
                 case CircleMembers.BLACKLIST:
                     groupInfo = new StickySectionDecoration.GroupInfo(3,
                             String.format(Locale.getDefault(), mActivity.getString(R.string
-                                    .circle_blacklist_format), mFrouLengh[3]));
+                                    .circle_blacklist_format), mFrouLengh[3]+""));
                     groupInfo.setPosition(position);
                     groupInfo.setGroupLength(mFrouLengh[3]);
                     break;
